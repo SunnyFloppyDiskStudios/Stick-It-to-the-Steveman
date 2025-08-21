0508d453e1afdbeb9dc34cf305ecdc2cc4b97be1
SunnyFlops <arav.prasad@outlook.com>
2025-08-21 19:24:46 +1200

    some changes and build

---
+++ src/main/java/net/sunnyflops/sittsm/StickItToTheStevemanClient.java
@@ -0,0 +1,9 @@
+package net.sunnyflops.sittsm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StickItToTheStevemanClient implements ClientModInitializer {
+    @Override public void onInitializeClient() {
+
+    }
+}
